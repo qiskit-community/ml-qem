46b1074bb2ff6fb8186eaafbd047bd16e46605e3
IceKhan13 <iskandar.sitdikov@gmail.com>
2023-04-28 20:42:22 -0400

    Naming

---
+++ blackwater/data/dataio/dataio.py
@@ -0,0 +1,55 @@
+"""IO."""
+
+import json
+from typing import List
+
+from blackwater.data.dataclasses import BlackwaterData, ExpValData
+
+
+# pylint: disable=unspecified-encoding
+class Writer:
+    """Base writer class."""
+
+    def save_to_file(self, path: str, data: List[BlackwaterData]):
+        """Writes data to file.
+
+        Args:
+            path: path of file to write to
+            data: data to write
+        """
+        raise NotImplementedError
+
+
+class Reader:
+    """Base reader class."""
+
+    def read_from_file(self, path: str) -> List[BlackwaterData]:
+        """Reads from file.
+
+        Args:
+            path: path to file to read from.
+
+        Returns:
+            list of data entries
+        """
+        raise NotImplementedError
+
+
+class ExpValDataWriter(Writer):
+    """ExpValDataWriter."""
+
+    def save_to_file(self, path: str, data: List[BlackwaterData]):
+        with open(path, "w") as file:
+            json.dump([entry.serialize() for entry in data], file, indent=4)
+
+
+class ExpValDataReader(Reader):
+    """ExpValDataReader."""
+
+    def read_from_file(self, path: str) -> List[BlackwaterData]:
+        result = []
+        with open(path, "r") as file:
+            json_data = json.load(file)
+            for entry in json_data:
+                result.append(ExpValData.deserialize(entry))
+        return result
