6d56f3fb00cbc6cdc81084680e4924097a402272
Haoran Liao <haoran.liao@berkeley.edu>
2023-06-06 14:05:03 -0400

    update

--- docs/tutorials/mlp.py
+++ docs/tutorials/mlp.py
@@ -145,6 +145,55 @@ def recursive_dict_loop(my_dict, parent_key=None, out=None, target_key1=None, ta
     return out or 0.
 
 
+def encode_data_v2_ecr(circuits, ideal_exp_vals, noisy_exp_vals, obs_size, meas_bases=None):
+    if isinstance(noisy_exp_vals[0], list) and len(noisy_exp_vals[0]) == 1:
+        noisy_exp_vals = [x[0] for x in noisy_exp_vals]
+
+    if meas_bases is None:
+        meas_bases = [[]]
+
+    gates_set = ['ecr', 'sx', 'x', 'id', 'rz']
+
+    vec = []
+
+    bin_size = 0.05 * np.pi
+    num_angle_bins = int(np.ceil(4 * np.pi / bin_size))
+
+    X = torch.zeros([len(circuits), len(vec) + len(gates_set) + num_angle_bins + obs_size + len(meas_bases[0])])
+
+    vec_slice = slice(0, len(vec))
+    gate_counts_slice = slice(len(vec), len(vec)+len(gates_set))
+    angle_bins_slice = slice(len(vec)+len(gates_set), len(vec)+len(gates_set)+num_angle_bins)
+    exp_val_slice = slice(len(vec)+len(gates_set)+num_angle_bins, len(vec)+len(gates_set)+num_angle_bins+obs_size)
+    meas_basis_slice = slice(len(vec)+len(gates_set)+num_angle_bins+obs_size, len(X[0]))
+
+    # X[:, vec_slice] = vec[None, :]
+
+    for i, circ in enumerate(circuits):
+        gate_counts_all = circ.count_ops()
+        X[i, gate_counts_slice] = torch.tensor(
+            [gate_counts_all.get(key, 0) for key in gates_set]
+        ) * 0.01  # put it in the same order of magnitude as the expectation values
+
+    for i, circ in enumerate(circuits):
+        gate_counts = count_gates_by_rotation_angle(circ, bin_size)
+        X[i, angle_bins_slice] = torch.tensor(gate_counts) * 0.01  # put it in the same order of magnitude as the expectation values
+
+        if obs_size > 1: assert len(noisy_exp_vals[i]) == obs_size
+        elif obs_size == 1: assert isinstance(noisy_exp_vals[i], float)
+
+        X[i, exp_val_slice] = torch.tensor(noisy_exp_vals[i])
+
+    if meas_bases != [[]]:
+        assert len(meas_bases) == len(circuits)
+        for i, basis in enumerate(meas_bases):
+            X[i, meas_basis_slice] = torch.tensor(basis)
+
+    y = torch.tensor(ideal_exp_vals, dtype=torch.float32)
+
+    return X, y
+
+
 
 def encode_data(circuits, properties, ideal_exp_vals, noisy_exp_vals, num_qubits, meas_bases=None):
     if isinstance(noisy_exp_vals[0], list) and len(noisy_exp_vals[0]) == 1:
